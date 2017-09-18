e9604f0ab35abaef1a51925d8d9ea53ccf101387
Steven Kelsey <steven.kelsey@revature.com>
2017-09-18 15:29:47 -0400

    First java project example, hello world

---
+++ Core_Java/FirstJavaProject/src/com/revature/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+public class HelloWorld {
+
+	//Entry Point of a Java Application, Only one
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		return;
+	}
+	
+
+}
+
