dbbcf823cfb13bdd90d1c7110c71c58e1a84ce86
Jay Wang <jaywang007@yahoo.com>
2017-10-04 11:24:00 -0400

    Refactor boiler plate codes and placed it into a method

--- Jay_Wang_Code/BankingApplication/src/main/java/com/revature/main/MainClass.java
+++ Jay_Wang_Code/BankingApplication/src/main/java/com/revature/main/MainClass.java
@@ -1,6 +1,7 @@
 package com.revature.main;
 
 import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import com.revature.exceptions.UserInfoException;
@@ -17,6 +18,13 @@ public class MainClass {
 											+ "[1]Deposit\n" 
 											+ "[2]Withdrawal\n"
 											+ "[0]Exit";
+	
+	private final static String mainMenuMessage = "Enter your User ID, first name and"
+											+ " last name to view account and balance";
+	
+	private final static String optionMessage = "Enter your account ID and amount to continue";
+	
+	private final static String finalMessage = "Enter 1 to continue or enter 0 to logout";
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Virtual Bank!");
@@ -67,28 +75,41 @@ public class MainClass {
 	
 	public void mainMenu() {
 		int exit = 1;
-		System.out.println("Enter your User ID, first name and last name to view account and balance");
-		int bankUserID = sc.nextInt();
-		String firstName = sc.next();
-		String lastName = sc.next();
-		BankUser user = new BankUser(bankUserID, firstName, lastName);
-		service.getBankAccountByUser(user);
-		while(exit != 0) {
-			System.out.println(screenMessage);
-			int option = sc.nextInt();
-			if(option == 1) {
-				System.out.println("Enter your account ID and amount to continue\t");
-				int accountID = sc.nextInt();
-				double amount = sc.nextDouble();
-				bankTransaction = new BankTransaction(bankUserID, accountID, option, amount, new Date());
-				service.performTransaction(bankTransaction);
-			} else if (option == 2) {
-				System.out.println("Enter your account ID and amount to continue\t");
-				int accountID = sc.nextInt();
-				double amount = sc.nextDouble();
-				bankTransaction = new BankTransaction(bankUserID, accountID, option, amount, new Date());
-				service.performTransaction(bankTransaction);
+		int logout = 1;
+		try {
+			while(logout != 0) {
+				System.out.println(mainMenuMessage);
+				int bankUserID = sc.nextInt();
+				String firstName = sc.next();
+				String lastName = sc.next();
+				BankUser user = new BankUser(bankUserID, firstName, lastName);
+				service.getBankAccountByUser(user);
+				while(exit != 0) {
+					System.out.println(screenMessage);
+					int option = sc.nextInt();
+					if(option == 1) {
+						transaction(user, option);
+					} else if (option == 2) {
+						transaction(user, option);
+					} else if (option == 0){
+						exit = 0;
+					} else {
+						System.out.println("Please enter a valid option");
+					}
+				}
+				System.out.println(finalMessage);
+				logout = sc.nextInt();	
 			}
+		}catch (InputMismatchException e) {
+				System.err.println("Sorry, invalid input.");
 		}
+	}
+	
+	public void transaction(BankUser user, int option) {
+		System.out.println(optionMessage);
+		int accountID = sc.nextInt();
+		double amount = sc.nextDouble();
+		bankTransaction = new BankTransaction(user.getBankUserID(), accountID, option, amount, new Date());
+		service.performTransaction(bankTransaction);
 	}
 }
