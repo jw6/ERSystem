e95e52d59d0d1af676bf19062329c8c9e5fe88ed
Jay Wang <jaywang007@yahoo.com>
2017-10-11 09:30:46 -0400

    rewrite reimbursement

--- Jay_Wang_Code/ERSystem/src/main/java/com/revature/models/Reimbursement.java
+++ Jay_Wang_Code/ERSystem/src/main/java/com/revature/models/Reimbursement.java
@@ -1,5 +1,110 @@
 package com.revature.models;
 
-public class Reimbursement {
+import java.util.Date;
 
+public class Reimbursement {
+	private int reimbursementID;
+	private ReimbursementStatus statusID;
+	private ReimbursementType typeID;
+	private double amount;
+	private Date submitted;
+	private Date resolved;
+	private String description;
+
+	public Reimbursement() {
+		super();
+	}
+	
+	public Reimbursement(int reimbursementID, ReimbursementStatus statusID, ReimbursementType typeID, double amount,
+			Date submitted, Date resolved, String description) {
+		super();
+		this.reimbursementID = reimbursementID;
+		this.statusID = statusID;
+		this.typeID = typeID;
+		this.amount = amount;
+		this.submitted = submitted;
+		this.resolved = resolved;
+		this.description = description;
+	}
+	
+	@Override
+	public String toString() {
+		return "Reimbursement [reimbursementID=" + reimbursementID + ", statusID=" + statusID + ", typeID=" + typeID
+				+ ", amount=" + amount + ", submitted=" + submitted + ", resolved=" + resolved + ", description="
+				+ description + "]";
+	}
+
+
+	public int getReimbursementID() {
+		return reimbursementID;
+	}
+
+
+	public void setReimbursementID(int reimbursementID) {
+		this.reimbursementID = reimbursementID;
+	}
+
+
+	public ReimbursementStatus getStatusID() {
+		return statusID;
+	}
+
+
+	public void setStatusID(ReimbursementStatus statusID) {
+		this.statusID = statusID;
+	}
+
+
+	public ReimbursementType getTypeID() {
+		return typeID;
+	}
+
+
+	public void setTypeID(ReimbursementType typeID) {
+		this.typeID = typeID;
+	}
+
+
+	public double getAmount() {
+		return amount;
+	}
+
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+
+	public Date getSubmitted() {
+		return submitted;
+	}
+
+
+	public void setSubmitted(Date submitted) {
+		this.submitted = submitted;
+	}
+
+
+	public Date getResolved() {
+		return resolved;
+	}
+
+
+	public void setResolved(Date resolved) {
+		this.resolved = resolved;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+
+	
 }
