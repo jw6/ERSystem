e69366c2198a7f9cf03d0c3b8dada495648667dc
Jay Wang <jaywang007@yahoo.com>
2017-10-16 23:35:08 -0400

    css styling with employee info display

--- Jay_Wang_Code/Project1/src/main/webapp/resources/js/employee.js
+++ Jay_Wang_Code/Project1/src/main/webapp/resources/js/employee.js
@@ -8,23 +8,23 @@ window.onload = function() {
 function loadNavbar() {
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function() {
-		if(xhr.readyState == 4 && xhr.status == 200) {
+		if (xhr.readyState == 4 && xhr.status == 200) {
 			navbar = xhr.responseText;
 			document.getElementById('employeeNavbar').innerHTML = navbar;
 		}
 	}
-	
+
 	xhr.open('GET', 'employeeNavBar', true);
 	xhr.send();
 }
 
 function loadEmployeeHomeView() {
-	var xhr = new XMLHttpRequest() 
+	var xhr = new XMLHttpRequest()
 	xhr.onreadystatechange = function() {
-		if(xhr.readyState == 4 && xhr.status == 200) {
+		if (xhr.readyState == 4 && xhr.status == 200) {
 			homeView = xhr.responseText();
 			document.getElementById('employeeHomeView').innerHTML = homeView;
-//			getEmployeeInfo();
+			// getEmployeeInfo();
 		}
 	}
 	xhr.open('GET', 'employeeHomeView', true);
@@ -35,7 +35,7 @@ var employee = {};
 function getEmployeeInfo() {
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function() {
-		if(xhr.readyState == 4 && xhr.status == 200) {
+		if (xhr.readyState == 4 && xhr.status == 200) {
 			employee = JSON.parse(xhr.responseText);
 			document.getElementById('employeeFirstName').innerHTML = employee.firstname;
 			document.getElementById('employeeLastName').innerHTML = employee.lastname;
